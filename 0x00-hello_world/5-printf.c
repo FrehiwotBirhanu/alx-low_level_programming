23bf72767106670b8c60e81cf69259f588c19ec5
Your Name <you@example.com>
2023-02-18 10:08:44 -0800

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*
- * main - Entry
- * Return: Always 0
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
  */
 int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
